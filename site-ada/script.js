f3586c5f6b815f4471cb8da106ace8724fc36c39
Teresa Campos <166440846+teresaccampos@users.noreply.github.com>
2024-09-28 22:30:57 -0300

    Delete site-ada directory

--- site-ada/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
-
-function validaFormulario(){
-  if(document.getElementById("name").value != "" && document.getElementById("email").value != "" && document.getElementById("numero").value != ""){
-    alert("Prontinho, você receberá as novidades por email.")
-  }else{
-    alert("Por favor, preencha os campos nome, email e telefone!")
-  }
-}
